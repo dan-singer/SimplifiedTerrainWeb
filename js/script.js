7984217a5408141faa9e3836718a80c9be390ff3
Dan Singer <djs5435@g.rit.edu>
2017-08-11 18:06:45 -0400

    Improved ui and made unity window into a popup system.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 
-window.onload = function(){
+var viewingGL = false;
 
-};
+
+function HandleSceneLoaded(){
+    document.getElementById("loader").style.display = "none";
+}
 
 document.onclick = function(event){
-    if (event.target.nodeName == "INPUT"){
-        gameInstance.SendMessage("Terrain", "FromJS_SetWebGLInput", 0);
+
+    if (viewingGL){
+        if (event.target.nodeName != "CANVAS"){
+            document.getElementById("unity").style.display = "none";            
+            viewingGL = false;
+        }
     }
     else{
-        gameInstance.SendMessage("Terrain", "FromJS_SetWebGLInput", 1);        
+        if (event.target.nodeName == "INPUT"){
+            gameInstance.SendMessage("Terrain", "FromJS_SetWebGLInput", 0);
+        }
+        else{
+            gameInstance.SendMessage("Terrain", "FromJS_SetWebGLInput", 1);        
+        }
     }
 };
 
@@ -31,8 +43,13 @@ function ValidateInputsThenApply()
     if (foundErr)
         return;
 
+    document.getElementById("unity").style.display = "block";
+
     //Send the urls to unity
     gameInstance.SendMessage("Terrain", "FromJS_LoadHeightmap", inputs[0].value);
     gameInstance.SendMessage("Terrain", "FromJS_LoadTerrainTex", inputs[1].value);
+
+    //To make we don't immediately close the unity popup window when setting viewingGL to true
+    setTimeout(function(){viewingGL = true;}, 1000);
     
 }
