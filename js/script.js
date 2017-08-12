9ae2489d03ae954b110b144b61bc1daaafbc4719
Dan Singer <djs5435@g.rit.edu>
2017-08-12 18:03:52 -0400

    Additional controls and bug fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
 
 var viewingGL = false;
-
+var justPressedReturn = false;
+var justPressedAlt = false;
 
 function HandleSceneLoaded(){
     document.getElementById("loader").style.display = "none";
+
+    document.addEventListener("pointerlockchange", function(event){
+        if (document.pointerLockElement == null){
+            if (!justPressedReturn && !justPressedAlt)
+                ResetAndHidePlayer(true);       
+        }
+    });
+
+
+    document.onkeydown = function(event){
+        justPressedAlt = true;
+        setTimeout(function(){justPressedAlt = false;}, 1000);
+    }
+
 }
+
+function HandleEscapePressedFromWebGL(){
+    ResetAndHidePlayer(false);
+}
+
+function HandleReturnPressedFromWebGL(){
+    console.log("Pressed return");
+    justPressedReturn = true;
+    setTimeout(function(){justPressedReturn = false;}, 1000);
+}
+
+
+function ResetAndHidePlayer(sendResetMessage){
+    if (sendResetMessage)
+        gameInstance.SendMessage("Terrain", "FromJS_Reset");    
+    document.getElementById("unity").style.display = "none";
+    //Some weird hack we need to do to get button presses to register again 
+    document.getElementById("input_area").click();
+    viewingGL = false;
+}
+
 
 document.onclick = function(event){
 
@@ -50,6 +86,6 @@ function ValidateInputsThenApply()
     gameInstance.SendMessage("Terrain", "FromJS_LoadTerrainTex", inputs[1].value);
 
     //To make we don't immediately close the unity popup window when setting viewingGL to true
-    setTimeout(function(){viewingGL = true;}, 1000);
+    setTimeout(function(){viewingGL = true;}, 100);
     
 }
